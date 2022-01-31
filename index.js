04fca705648a9941e47a8f4cdcbf2e8220f8e397
Axel Aquino <aquino.axel@gmail.com>
2022-01-31 13:54:17 -0500

    commit tree 7276064a47e2dfe8990ecc5059a295b1084b1d89

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Code your solution in this file!
+
+
+function distanceFromHqInBlocks(pickBlock){
+    const hqLocation = 42;
+    return Math.abs(pickBlock - hqLocation);
+}
+
+
+
+function distanceFromHqInFeet(feetFromHq) {
+    const feet = distanceFromHqInBlocks(feetFromHq);
+    return feet * 264;
+
+}
+
+
+function distanceTravelledInFeet(start, destination) {
+    const manhattanBlockInFeet = 264;
+    return Math.abs(destination - start) * manhattanBlockInFeet;
+}
+
+function calculatesFarePrice(start, destination) {
+    const data = distanceTravelledInFeet(start, destination);
+
+    if(data < 400){
+        return 0
+    } else if (data > 2500) {
+        return 'cannot travel that far';
+    } else if (data >= 2000) {
+        return 25
+    } else if (data < 2000) {
+        return ((data) - 400) * .02 
+    }
+
+}
